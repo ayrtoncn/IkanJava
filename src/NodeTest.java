25818b05d45e05485f5c508b70b056adb5b98a3d
ayrtocn <ayrton.niwarlangga@gmail.com>
2018-04-24 16:59:04 +0700

    LinkedList,Node,point junit test

---
+++ src/NodeTest.java
@@ -0,0 +1,46 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NodeTest {
+
+  @Test
+  void constructorTest() {
+    Node<Integer> newNode = new Node<Integer>(10);
+    assertEquals((Integer)10, newNode.getData(), "The Node data should be 10");
+  }
+
+  @Test
+  void cctorTest() {
+    Node<Integer> newNode = new Node<Integer>(12);
+    Node<Integer> anotherNode = new Node<Integer>(newNode);
+    assertEquals((Integer)12, anotherNode.getData(), "The copy constructor is not working");
+  }
+  
+  @Test
+  void getDataTest() {
+    Node<Integer> newNode = new Node<Integer>(12);
+    assertEquals((Integer)12, newNode.getData(), "The getData is not working");
+  }
+  
+  @Test
+  void getNextTest() {
+    Node<Integer> newNode = new Node<Integer>(12);
+    Node<Integer> anotherNode = new Node<Integer>(10);
+    anotherNode.setNext(newNode);
+    assertEquals((Integer)12, anotherNode.getNext().getData(), "The getNext is not working");
+  }
+  
+  @Test
+  void setDataTest() {
+    Node<Integer> newNode = new Node<Integer>(12);
+    newNode.setData(10);
+    assertEquals((Integer)10, newNode.getData(), "The setData is not working");
+  }
+  
+  @Test
+  void setNextTest() {
+    
+  }
+}
---
+++ src/PointTest.java
@@ -0,0 +1,55 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PointTest {
+
+  @Test
+  void testPoint() {
+    Point p = new Point();
+    assertEquals(-99,p.getAbsis());
+    assertEquals(-99,p.getOrdinat());
+  }
+
+  @Test
+  void testPointDoubleDouble() {
+    Point p = new Point(10,10);
+    assertEquals(10,p.getAbsis());
+    assertEquals(10,p.getOrdinat());
+  }
+
+  @Test
+  void testGetOrdinat() {
+    Point p = new Point(10,10);
+    assertEquals(10,p.getOrdinat());
+  }
+
+  @Test
+  void testSetOrdinat() {
+    Point p = new Point(10,10);
+    p.setOrdinat(5);
+    assertEquals(5,p.getOrdinat());
+  }
+
+  @Test
+  void testGetAbsis() {
+    Point p = new Point(10,10);
+    assertEquals(10,p.getAbsis());
+  }
+
+  @Test
+  void testSetAbsis() {
+    Point p = new Point(10,10);
+    p.setAbsis(5);
+    assertEquals(5,p.getAbsis());
+  }
+
+  @Test
+  void testGetDistance() {
+    Point p1 = new Point(3,4);
+    Point p2 = new Point(0,0);
+    assertEquals(5,p1.getDistance(p2));
+  }
+
+}
